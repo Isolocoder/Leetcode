b285558cca6f116095c7f85663173fa7fd3c4be2
Isolocoder <46982753+Isolocoder@users.noreply.github.com>
2021-03-21 20:01:11 +0800

    455fix

--- src/main/java/com/soulostar/findContentChildren.java
+++ src/main/java/com/soulostar/findContentChildren.java
@@ -10,24 +10,23 @@ public class findContentChildren {
         int []s = new int[]{1,1};
         System.out.println(solution.findContentChildren(g,s));
     }
-}
 
-
-class Solution {
-    public int findContentChildren(int[] g, int[] s) {
-        Arrays.sort(g);
-        Arrays.sort(s);
-        int cnt = 0;
-        int gp = g.length-1,sp = s.length-1;
-        while (gp>=0&&sp>=0){
-            if (g[gp] <= s[sp]){
-                gp--;
-                sp--;
-                cnt++;
-            }else {
-                gp--;
+    static class Solution {
+        public int findContentChildren(int[] g, int[] s) {
+            Arrays.sort(g);
+            Arrays.sort(s);
+            int cnt = 0;
+            int gp = g.length-1,sp = s.length-1;
+            while (gp>=0&&sp>=0){
+                if (g[gp] <= s[sp]){
+                    gp--;
+                    sp--;
+                    cnt++;
+                }else {
+                    gp--;
+                }
             }
+            return cnt;
         }
-        return cnt;
     }
 }
